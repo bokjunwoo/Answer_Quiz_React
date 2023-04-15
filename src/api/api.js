be52e1e31c44451149b32ae66fe57e2c089a36a6
JUNU <bokjunwoo@gmail.com>
2023-04-15 21:28:58 +0900

    fix : baseURL 변경

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://54.180.136.213:3001/mongo',
+  baseURL: 'http://3.39.192.108:3001/mongo',
   headers: {
     'Content-Type': 'application/json',
   },
